4c0ef8ee34b0890a498ae19767a4b1a9a220f2f6
jimpeeters97 <peetersjim97@gmail.com>
2017-12-11 14:43:16 +0100

    Newest attempt to fix

--- target/dockerfile/run.sh
+++ target/dockerfile/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar /usr/local/les10taakdestinationservice/les10taakdestinationservice-0.1.0.jar
+java -jar /usr/local/les10taakdestinationservice/les10taakdestinationservice-latest.jar
